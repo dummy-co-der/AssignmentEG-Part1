34ccdf29f9475df85046557895cb59d0d2ae479a
dummy-co-der <maheshwarianuj1001@gmail.com>
2023-07-15 14:25:31 +0530

    task1

--- src/App.jsx
+++ src/App.jsx
@@ -12,43 +12,55 @@ const App = () => {
   const [images, setImages] = useState([]);
   const [selectedImageIndex, setSelectedImageIndex] = useState(null);
   const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    fetchImages();
-  }, []);
+  const [offset, setoffset] = useState(0);
+  const [flag, setflag] = useState(false);
+  const [data, setData] = useState([]);
 
   const fetchImages = async () => {
     try {
       setLoading(true);
       const response = await axios.get(
-        `https://api.slingacademy.com/v1/sample-data/photos?limit=${IMAGES_PER_SCROLL}`
+        `https://api.slingacademy.com/v1/sample-data/photos?limit=${IMAGES_PER_SCROLL}&offset=${offset}`
       );
       const fetchedImages = response.data.photos.map((image, index) => ({
         id: image.id,
-        url: `http://via.placeholder.com/${THUMBNAIL_SIZE}x${THUMBNAIL_SIZE}?text=${
-          index + 1
-        }`,
-        fullSizeUrl: `http://via.placeholder.com/${FULL_SIZE}x${FULL_SIZE}?text=${
-          index + 1
-        }`,
-        downloadSizeUrl: `http://via.placeholder.com/${DOWNLOAD_SIZE}x${DOWNLOAD_SIZE}?text=${
-          index + 1
-        }`,
+        url: `http://via.placeholder.com/${THUMBNAIL_SIZE}x${THUMBNAIL_SIZE}?text=${image.id}`,
+        fullSizeUrl: `http://via.placeholder.com/${FULL_SIZE}x${FULL_SIZE}?text=${image.id}`,
+        downloadSizeUrl: `http://via.placeholder.com/${DOWNLOAD_SIZE}x${DOWNLOAD_SIZE}?text=${image.id}`,
       }));
-      setImages((prevImages) => [...prevImages, ...fetchedImages]);
+
+      if (offset === 0) {
+        setImages(fetchedImages);
+      } else {
+        setImages((prevImages) => [...prevImages, ...fetchedImages]);
+      }
+
+      setoffset(offset + 30);
       setLoading(false);
+      setData(response.data.photos);
     } catch (error) {
       console.error("Error fetching images:", error);
       setLoading(false);
     }
   };
 
+  useEffect(() => {
+    if (flag) {
+      fetchImages();
+      setflag(false);
+    }
+  }, [offset, flag]);
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  useEffect(() => {}, [data]);
+  console.log(offset);
   const handleScroll = () => {
     if (
       window.innerHeight + document.documentElement.scrollTop + 1 >=
       document.documentElement.scrollHeight
     ) {
-      fetchImages();
+      setflag(true);
     }
   };
 
@@ -117,7 +129,7 @@ const App = () => {
         {images.map((image, index) => (
           <div
             className="thumbnail"
-            key={image.id}
+            key={image.title}
             onClick={() => openImage(index)}
           >
             <img src={image.url} alt={`Image ${index + 1}`} />
